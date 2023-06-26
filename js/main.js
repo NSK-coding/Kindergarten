6d554e37aa708748bc644ef2ee4d9d7042fc7bd2
NSK-coding <137236133+NSK-coding@users.noreply.github.com>
2023-06-26 16:38:19 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -46,6 +46,57 @@ $(function(){
 });
 
 
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  $('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top+150; //要素より、150px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');
+    // 画面内に入ったらfadeInというクラス名を追記
+    }else{
+      $(this).removeClass('fadeUp');
+    // 画面外に出たらfadeInというクラス名を外す
+    }
+    });
+
+  $('.marchTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-350; //要素より、150px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('march');
+    $('.rabbit').addClass('march2');
+    $('.fox').addClass('march3');
+    $('.deer').addClass('march4');
+    
+    // 画面内に入ったらfadeInというクラス名を追記
+    }else{
+      $(this).removeClass('march');
+      $('.rabbit').removeClass('march2');
+      $('.fox').removeClass('march3');
+      $('.deer').removeClass('march4');
+    // 画面外に出たらfadeInというクラス名を外す
+    }
+  });
+
+
+
+}//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// // 画面が読み込まれたらすぐに動かしたい場合の記述
+// $(window).on('load', function(){
+//   fadeAnime();/* アニメーション用の関数を呼ぶ*/
+// });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
 
 
 
